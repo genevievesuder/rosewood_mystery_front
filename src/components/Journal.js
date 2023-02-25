80d530428d05e2f79cc0804f233f204277e59efe
Genevieve Suder <genevievesuder@gmail.com>
2023-02-25 14:13:24 -0800

    journal created

--- src/components/Journal.js
+++ src/components/Journal.js
@@ -3,8 +3,9 @@ import React from 'react'
 const Journal = () => {
   return (
     <div className="journal-container">
-    <img style={{objectFit:"fill", minWidth:"70%"}}  src={process.env.PUBLIC_URL+"/journal.jpg"} alt="books"/>
-  </div>
+      <div className="journal-left"></div>
+      <div className="journal-right"></div>
+    </div>
   )
 }
 
