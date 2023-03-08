fbc5661d5f82ca25bddd165ab283dd855425d845
Genevieve Suder <genevievesuder@gmail.com>
2023-03-08 13:48:13 -0800

    final room css

--- src/components/FinalRoom.js
+++ src/components/FinalRoom.js
@@ -8,11 +8,8 @@ import { NotifContext } from "../context/NotifContext";
 const FinalRoom = () => {
     const {notif, setNotif} = useContext(NotifContext)
     const {user, setUser} = useContext(UserContext)
-    const [openClue1, setOpenClue1] = useState(false)
-    const [note, setNote] = useState(false)
     const navigate = useNavigate()
-    const [flipImg, setFlipImg] = useState(false)
-
+  
     
     const handleClick = async (clue_id) => {
         const resp = await Clue(clue_id)
@@ -20,44 +17,15 @@ const FinalRoom = () => {
         !!resp.id ? setUser(resp) : setNotif(resp)
     }
 
-    const open1 = () => {
-        setOpenClue1(current => !current)
-        setFlipImg(false)
-        // if (!openClue1) {
-        //     handleClick(6)
-        // }
-    }
-
-    const handleFlip = () => {
-      setFlipImg(current => !current)
-      handleClick(6)
-      setNotif("What the...")
-  }
-
-    if (openClue1) {
-        return (
-        <div className="home-container">
-             <button onClick={open1} className="clue-back-btn">x</button>
-       <button onClick={handleFlip} className="grab-note"></button>
-      {!flipImg ? (
-        <img className="home-view" src={process.env.PUBLIC_URL+"/thermostat.png"} alt="thermostat"/>
-      ) : (
-        <img className="home-view" src={process.env.PUBLIC_URL+"/lmo.png"} alt="let me out"/>
-      )}
-        </div>)
-    }
- 
 
 
  
     if (!user) return <h1>...loading</h1>
   return (
-    <div className="home-container">
+    <div className="final-room-container">
         <BackBtn />
-        { user.clues.length >= 5 && user.hints.length >= 3 ? (
-        <button onClick={open1} className="backroom-clue-btn"></button>
-        ) : null}
-        <img className="home-view" src={process.env.PUBLIC_URL+"/backroom.png"} alt="viewfromdesk"/>
+       <div className="frc"></div>
+        <img className="home-view" src={process.env.PUBLIC_URL+"/finalroom.jpg"} alt="a spooky room with something glowing in the corner..."/>
     </div>
   )
 }
